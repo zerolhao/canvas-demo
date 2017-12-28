0d2cd5a18e2a5f394f5585a6219a29e23572a813
zerolhao <zerolhao@gmail.com>
2017-12-28 19:34:06 +0800

    添加画笔、颜色、粗细、清空、下载功能

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
 var canvas = document.getElementById('canvas')
-autoSetCanvasSize()
-
 var context = canvas.getContext('2d')
+
+autoSetCanvasSize()  // 设置默认canvas宽高
+autoSetCanvasBgC()  // 涩会直默认背景颜色
+setPenColor("black") // 设置默认画笔颜色
+setLineWidth(2) // 设置默认画笔粗细
+
 var using = false // 设置画板使用开关
 var lastPoint = { // 坐标
   x: undefined,
   y: undefined
 }
+
 // 特性检测，此检测是否支持触屏
 if (document.body.ontouchstart !== undefined) {
   // 触屏设备
   canvas.ontouchstart = function(e) {
-    console.log("touch start")
     var x = e.touches[0].clientX
     var y = e.touches[0].clientY
     using = true
@@ -25,7 +29,6 @@ if (document.body.ontouchstart !== undefined) {
     }
   }
   canvas.ontouchmove = function(e) {
-    console.log("touch move")
     var x = e.touches[0].clientX
     var y = e.touches[0].clientY
     if (!using) { return }
@@ -35,10 +38,9 @@ if (document.body.ontouchstart !== undefined) {
       drawLine(lastPoint.x, lastPoint.y, x, y)
       lastPoint.x = x // 更新坐标
       lastPoint.y = y
-    } 
+    }
   }
   canvas.ontouchend = function(e) {
-    console.log("touch end")
     using = false
   }
 } else {
@@ -76,12 +78,61 @@ if (document.body.ontouchstart !== undefined) {
 var eraserEnable = false
 var eraser = document.getElementById('eraser')
 eraser.onclick = function() {
-  eraserEnable = !eraserEnable
+  eraserEnable = true
+  this.classList.add('active')
+  pen.classList.remove('active')
 }
+// 画笔
+var pen = document.getElementById('pen')
+pen.onclick = function() {
+  eraserEnable = false
+  this.classList.add('active')
+  eraser.classList.remove('active')
+}
+
+// 画笔颜色列表
+var colorList = document.getElementById('colorList')  // 颜色列表
+colorList.onclick = function() {
+  this.classList.add('active')
+}
+var activeColor = document.getElementById('activeColor') // 当前画笔颜色
+var red = document.getElementById('red')
+var green = document.getElementById('green')
+var blue = document.getElementById('blue')
+var black = document.getElementById('black')
+red.addEventListener('click', changeColor)  // 更改画笔颜色及显示
+green.addEventListener('click', changeColor)
+blue.addEventListener('click', changeColor)
+black.addEventListener('click', changeColor)
+
+canvas.addEventListener('click', closeColorList)  // 点击canvas也可以关闭颜色列表
+
+// 画笔粗细
+var lineWidth = document.getElementById('lineWidth')
+lineWidth.onclick = function(){
+  var width = prompt("请输入画笔大小（比如：2、5、10）")
+  setLineWidth(width)
+}
+// 清空canvas
+var clear = document.getElementById('clear')
+clear.onclick = function(){
+  context.clearRect(0, 0, canvas.width, canvas.height)
+}
+
+// 将图画下载
+var download = document.getElementById('download')
+download.onclick = function(){
+  var url = canvas.toDataURL()
+  var a = document.createElement('a')
+  a.href = url
+  a.download = "name.jpg"
+  a.click()
+}
+
 
 /******工具函数******/
 
-// 默认设置canvas宽高
+// 设置默认canvas宽高
 function autoSetCanvasSize() {
   // 获取页面宽高
   var pageWith = document.documentElement.clientWidth
@@ -91,13 +142,41 @@ function autoSetCanvasSize() {
   canvas.height = pageHeight
 }
 
+// 涩会直默认背景颜色
+function autoSetCanvasBgC(){
+  context.fillStyle = "white"  // 设置填充颜色
+  context.fillRect(0, 0, canvas.width, canvas.height)  // 填充整个canvas
+}
+
 // 画线条
 function drawLine(lastX, lastY, newX, newY) {
   context.beginPath() // 新建路径
   context.moveTo(lastX, lastY) // 设置起始位置
-  context.lineWidth = 2 // 设置线条宽度
-  context.strokeStyle = 'black' // 设置线条颜色
+  //  context.lineWidth = 2 // 设置线条宽度
+  //  context.strokeStyle = 'black' // 设置线条颜色
   context.lineTo(newX, newY) // 绘制线条至new x，y坐标
   context.stroke() // 绘制线条
   context.closePath() // 闭合路径
+}
+// 设置画笔粗细
+function setLineWidth(width) {
+  context.lineWidth = width
+}
+// 设置颜色
+function setPenColor(color) {
+  context.strokeStyle = color
+}
+// 关闭颜色设置
+function closeColorList() {
+  if (colorList.className !== "colorList") {
+    colorList.classList.remove("active")
+  }
+}
+// 颜色列表-设置当前画笔颜色及显示
+function changeColor(e){
+  var color = this.id.toString()
+  setPenColor(color) // 设置画笔颜色
+  activeColor.style.backgroundColor = color  // 同时更改显示的颜色
+  closeColorList()  // 关闭颜色列表
+  e.stopPropagation() // 停止事件冒泡
 }
